5e3305d9d73c2befe1d67c5a0a8a270e54e841ae
Jarin Tabassum Anisa <142834496+Tabassum385@users.noreply.github.com>
2023-08-27 14:32:46 +0600

    Add files via upload

---
+++ Person_info.java
@@ -0,0 +1,18 @@
+
+
+
+  package Lab_work;
+
+public class Person_info 
+{
+     public static void main(String[] args)
+     {
+        
+        Person me = new Person ();
+        me.name = "Tabassum";
+        me.age = 21;
+        
+        System.out.println("My name = "+me.name);
+        System.out.println("My age = "+me.age);
+     }
+}
