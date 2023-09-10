9373f47c536f5f9cec361ec154db42757f260467
Jarin Tabassum Anisa <142834496+Tabassum385@users.noreply.github.com>
2023-09-11 03:13:44 +0600

    Add files via upload

---
+++ Triangle_Calculation.java
@@ -0,0 +1,15 @@
+
+package task_2;
+
+
+public class Triangle_Calculation {
+    
+    public static void main(String[] args) {
+        Triangle trianglearea = new Triangle(10, 17);
+        
+        System.out.println("The area of Triangle: " + trianglearea.getArea());
+    }
+}
+    
+    
+
